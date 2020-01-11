d76e1311df0bbaea872155db8885a6976dd57ae2
Abhinav Abhinav <abhinavwadhankar@Abhinavs-MacBook-Air.local>
2020-01-11 17:00:54 +0530

    SwapInterger

---
+++ AlgoSolutions/src/Entry/Test.java
@@ -0,0 +1,19 @@
+package src.Entry;
+
+
+import src.problems.*;
+
+public class Test {
+	
+	public static void main(String[] args) 
+	{
+		SwapLogic sl = new SwapLogic();
+		sl.swapbyAddSub(2, 3);
+		sl.swapbyBitwiseEXOR(2, 4);
+		sl.swapbyMulDiv(5, 7);
+
+	}
+	
+	
+
+}
--- AlgoSolutions/src/problems/SwapLogic.java
+++ AlgoSolutions/src/problems/SwapLogic.java
@@ -1,5 +1,57 @@
-package problems;
+package src.problems;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SwapLogic {
+	
+	public List<Integer> swapbyBitwiseEXOR(int a, int b)
+	{
+		 System.out.println("Before swapbyBitwiseEXOR a:-"+a + "b:-"+b);
+		 List<Integer> li=new ArrayList<Integer>();
+		 a=a^b;
+		 b=a^b;
+		 a=a^b;
 
+		 li.add(a);
+		 li.add(b);
+		 
+		 System.out.println("After swapbyBitwiseEXOR a:-"+a + "b:-"+b);
+		 
+		 return li;
+	}
+
+	public List<Integer> swapbyMulDiv(int a, int b)
+	{
+		
+		 System.out.println("Before swapbyMulDiv a:-"+a + "b:-"+b);
+		 
+		 List<Integer> li=new ArrayList<Integer>();
+		 a=a*b;
+		 b=a/b;
+		 a=a/b;
+
+		 li.add(a);
+		 li.add(b);
+	
+		 System.out.println("After swapbyMulDiv a:-"+a + "b:-"+b);
+		 
+		 return li;
+	}
+
+	public List<Integer> swapbyAddSub(int a, int b)
+	{
+		System.out.println("Before swapbyAddSub a:-"+a + "b:-"+b);
+		
+		 List<Integer> li=new ArrayList<Integer>();
+		 a=a+b;
+		 b=a-b;
+		 a=a-b;
+
+		 li.add(a);
+		 li.add(b);
+		 
+		 System.out.println("After swapbyAddSub a:-"+a + "b:-"+b);
+		 
+		 return li;
+	}
 }
